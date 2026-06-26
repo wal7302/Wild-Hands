9d280aaf54df5a38427dbc646a2656491f5a688a
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-25 21:29:01 -0500

    Update turn.py

--- engine/game/turn.py
+++ engine/game/turn.py
@@ -1,17 +1,18 @@
 class Turn:
 
-    def __init__(self, player, deck):
+    def __init__(self, player, round_state):
 
         self.player = player
-        self.deck = deck
+        self.round_state = round_state
         self.drawn_card = None
         self.discarded_card = None
 
     def draw(self):
 
-        self.drawn_card = self.deck.draw()
+        self.drawn_card = self.round_state.deck.draw()
 
         if self.drawn_card:
+            self.drawn_card.is_wild = self.drawn_card.rank == self.round_state.wild_rank
             self.player.draw(self.drawn_card)
 
         return self.drawn_card
@@ -19,5 +20,6 @@ class Turn:
     def discard(self, index):
 
         self.discarded_card = self.player.discard(index)
+        self.round_state.add_discard(self.discarded_card)
 
         return self.discarded_card
