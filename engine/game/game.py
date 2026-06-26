8ec9ccc17a01f83e18eae19882452c9455af6b93
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-26 09:07:58 -0500

    Update game.py

--- engine/game/game.py
+++ engine/game/game.py
@@ -1,4 +1,5 @@
 from engine.game.round import Round
+from engine.game.score import ScoreEngine
 
 
 class Game:
@@ -6,9 +7,7 @@ class Game:
     def __init__(self, players):
 
         self.players = players
-
         self.round_number = 3
-
         self.round = None
 
     def start_round(self):
@@ -23,5 +22,18 @@ class Game:
     def next_round(self):
 
         self.round_number += 1
-
         self.start_round()
+
+    def end_round(self):
+
+        scores = {}
+
+        for player in self.players:
+
+            score = ScoreEngine.best_score(player.hand)
+
+            player.total_score += score
+
+            scores[player.name] = score
+
+        return scores
