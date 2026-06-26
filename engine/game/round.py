5bd69abd78d4201801abcf354ecb483658f1f781
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-26 12:35:26 -0500

    Update round.py

--- engine/game/round.py
+++ engine/game/round.py
@@ -25,9 +25,33 @@ class Round:
 
             for player in self.players:
 
-                card = self.deck.draw()
-                card.is_wild = card.rank == self.wild_rank
-                player.draw(card)
+                card = self.draw_from_deck()
+
+                if card is not None:
+                    player.draw(card)
+
+    def draw_from_deck(self):
+
+        card = self.deck.draw()
+
+        if card is None:
+            self.reshuffle_discard_into_deck()
+            card = self.deck.draw()
+
+        if card is not None:
+            card.is_wild = card.rank == self.wild_rank
+
+        return card
+
+    def reshuffle_discard_into_deck(self):
+
+        if not self.discard_pile:
+            return
+
+        self.deck.cards = self.discard_pile.copy()
+        self.discard_pile.clear()
+        self.deck.shuffle()
+        self.deck.set_wild_rank(self.wild_rank)
 
     @property
     def current_player(self):
