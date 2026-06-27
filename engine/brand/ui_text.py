cac78d17c322e71f75c0787a3ce169a236db341a
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-26 19:30:57 -0500

    Create ui_text.py

---
+++ engine/brand/ui_text.py
@@ -0,0 +1,17 @@
+class UIText:
+
+    QUICK_MATCH = "Quick Match"
+    CLASSIC_MODE = "Classic"
+    FAMILY_GAME = "Family Game"
+    MY_GAME_ROOM = "My Game Room"
+    COLLECTIONS = "Collections"
+    PROFILE = "Profile"
+
+    PLAY_AGAIN = "Play Again"
+    SEND_GIFT = "Send Gift"
+    TABLE_TALK = "Table Talk"
+
+    ROUND_RESULTS = "Round Results"
+    MATCH_COMPLETE = "Match Complete"
+    CURRENT_LEADER = "Current Leader"
+    WINNER = "Winner"
