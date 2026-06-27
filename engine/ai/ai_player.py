510ecd02426299d4cc919700555b78f30526402f
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-26 19:17:33 -0500

    Update ai_player.py

--- engine/ai/ai_player.py
+++ engine/ai/ai_player.py
@@ -1,13 +1,15 @@
 from engine.models.player import Player
 from engine.ai.basic_ai import BasicAI
+from engine.ai.personality_library import PersonalityLibrary
 
 
 class AIPlayer(Player):
 
-    def __init__(self, name, personality="Basic"):
+    def __init__(self, name, personality="competitive_one"):
         super().__init__(name)
 
-        self.personality = personality
+        self.personality_id = personality
+        self.personality = PersonalityLibrary.find(personality)
         self.brain = BasicAI()
 
     def choose_discard_index(self):
