27b517ccfeba0181b01e07694cdffdff2bc249fd
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-26 19:30:38 -0500

    Create copy.py

---
+++ engine/brand/copy.py
@@ -0,0 +1,9 @@
+class BrandCopy:
+
+    GAME_TITLE = "Wild Hands"
+    TAGLINE = "Every Deal Changes the Game"
+    NORTH_STAR = "Losing should be just as fun as winning."
+
+    OPENING_LINE = "Looks like everyone's here, honey."
+    DEFAULT_WELCOME_SIGN = "Pull Up a Chair"
+    DEFAULT_FAMILY_MOTTO = "One more hand."
