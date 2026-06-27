42ea5f20e51f6ea6b8abfd76a88eb02488970efa
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-27 05:40:47 -0500

    Create TableAnimationDirector.gd

---
+++ godot/scripts/TableAnimationDirector.gd
@@ -0,0 +1,26 @@
+class_name TableAnimationDirector
+extends Node
+
+func animate_card_from_deck(card: Node2D, deck_position: Vector2, target_position: Vector2, duration: float = 0.35):
+	card.global_position = deck_position
+
+	var tween := create_tween()
+	tween.tween_property(card, "global_position", target_position, duration)
+	return tween
+
+func animate_card_to_discard(card: Node2D, discard_position: Vector2, duration: float = 0.35):
+	var tween := create_tween()
+	tween.tween_property(card, "global_position", discard_position, duration)
+	return tween
+
+func animate_hand_reach(hand: Node2D, from_position: Vector2, to_position: Vector2, duration: float = 0.3):
+	hand.show_hand(from_position)
+
+	var tween := create_tween()
+	tween.tween_property(hand, "global_position", to_position, duration)
+	return tween
+
+func animate_hand_return(hand: Node2D, to_position: Vector2, duration: float = 0.3):
+	var tween := create_tween()
+	tween.tween_property(hand, "global_position", to_position, duration)
+	return tween
