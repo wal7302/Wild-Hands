a4e9fdc1a84e9dba79023bb7f092c40919a0566e
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-26 22:21:00 -0500

    Create animation_renderer.py

---
+++ engine/console/animation_renderer.py
@@ -0,0 +1,36 @@
+class AnimationRenderer:
+
+    ANIMATIONS = {
+
+        "sip_wine":
+            "🍷 Grace smiles and takes a sip of her wine.",
+
+        "shuffle_cards":
+            "🃏 Grace shuffles the deck.",
+
+        "wave":
+            "👋 Grace waves.",
+
+        "laugh":
+            "😂 Grace laughs.",
+
+        "thinking":
+            "🤔 Grace quietly studies the table.",
+
+        "deal_cards":
+            "🂠 Grace begins dealing the cards.",
+
+        "raise_eyebrow":
+            "🤨 Grace raises an eyebrow.",
+
+        "smile":
+            "😊 Grace smiles.",
+    }
+
+    @staticmethod
+    def render(animation):
+
+        return AnimationRenderer.ANIMATIONS.get(
+            animation,
+            ""
+        )
