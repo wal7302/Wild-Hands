d337994949675fd68327e314d7944c4c0fdeda35
wal7302 <119759818+wal7302@users.noreply.github.com>
2026-06-27 01:10:55 -0500

    Create GameAudio.gd

---
+++ godot/scripts/GameAudio.gd
@@ -0,0 +1,14 @@
+class_name GameAudio
+extends Node
+
+func play_card_deal():
+	print("SFX: card_deal")
+
+func play_card_discard():
+	print("SFX: card_discard")
+
+func play_wild_discard():
+	print("SFX: wild_discard")
+
+func play_grace_reaction():
+	print("SFX: grace_reaction")
